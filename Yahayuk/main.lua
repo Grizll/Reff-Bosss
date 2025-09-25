49121ce7b0473ef74c984e24a3bb72a1c204d148
XuKrost <96784584+Grizll@users.noreply.github.com>
2025-09-25 07:45:57 +0700

    Update main.lua

--- Yahayuk/main.lua
+++ Yahayuk/main.lua
@@ -13,4 +13,5 @@ end
 
 -- Menjalankan kedua loadstring
 executeLoadstring("https://raw.githubusercontent.com/noirexe/TestEsceh/refs/heads/main/Fearless.lua", "Fearless Menus")
-executeLoadstring("https://raw.githubusercontent.com/Grizll/Reff-Bosss/refs/heads/main/Yahayuk/mainmap970.lua", "MainMap Script")
+executeLoadstring("https://raw.githubusercontent.com/Grizll/Reff-Bosss/refs/heads/main/Yahayuk/mainmap970.lua", "MainMap Script"),
+executeLoadstring("https://raw.githubusercontent.com/Grizll/Reff-Bosss/refs/heads/main/Yahayuk/Cours.lua", "Cours Ava")
